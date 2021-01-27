fd52eb51be034ed6eb10abcf08f517d33b85a933
Ife Oyelakin <oyeife@gmail.com>
2021-01-27 13:38:35 +0100

    Update first_script.sh

--- Scripts/Linux Command/first_script.sh
+++ Scripts/Linux Command/first_script.sh
@@ -1,5 +1,16 @@
+# Create an empty file with your favourite editor on the command line, copy the script below and save the file with the extension(.sh)
+
 #!/bin/bash
 
 # My first script
 
 echo "Hello World!"
+
+# Set the file to be executable using chmod command 
+# Replace [FILE_NAME] with a name you've chosen to save your file with. 
+
+chmod 755 [FILE_NAME]
+
+# Run your script 
+
+./[FILE_NAME]
